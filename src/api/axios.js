cd235ef4dd349db4fe3bd2eaf32e7176c7969981
Ninh Pham <ninhninh2906@gmail.com>
2025-12-10 17:06:00 +0700

    Connect frontend to Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ia07-jwt-xxvs.onrender.com/api';
 
 // Create axios instance
 const apiClient = axios.create({
